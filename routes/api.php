24a436c5221a262b87c3d89f2ea3f127dfd0e59f
Thomas <azzpr@protonmail.com>
2022-05-28 19:59:01 +0200

    added a post model, and added a postcontroller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PostsController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,17 +23,17 @@ use Illuminate\Support\Facades\Route;
 Route::post('/user/register', [AuthController::class,  'register']);
 Route::post('/user/login', [AuthController::class,  'login']);
 
-// // Products
-// Route::get('/products',  [ProductController::class, 'index']);
-// Route::get('/products/{id}',  [ProductController::class, 'show']);
-// Route::get('products/search/{name}', [ProductController::class, 'search']);
+// // Posts
+Route::get('/posts',  [PostsController::class, 'index']);
+Route::get('/posts/{id}',  [PostsController::class, 'show']);
+Route::get('posts/search/{name}', [PostsController::class, 'search']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     // Auth
     Route::post('/user/logout', [AuthController::class,  'logout']);
-    // // Products
-    // Route::post('/products', [ProductController::class,  'store']);
-    // Route::put('/products/{id}', [ProductController::class,  'update']);
-    // Route::delete('/products/{id}', [ProductController::class,  'destroy']);
+    // // Posts
+    Route::post('/posts/upload', [PostsController::class,  'store']);
+    Route::put('/posts/{id}', [PostsController::class,  'update']);
+    Route::delete('/posts/{id}', [PostsController::class,  'destroy']);
 });
